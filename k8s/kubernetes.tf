70cd78834d7dfd695d2c7f1c7404bbdd8568a09d
Your Name <you@example.com>
2020-12-18 02:29:47 -0500

    first commit

---
+++ k8s/kubernetes.tf
@@ -0,0 +1,74 @@
+# # Kubernetes provider
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
+
+provider "kubernetes" {
+  load_config_file       = "false"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-app"
+    labels = {
+      App = "Frontend"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "Frontend"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "Frontend"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "nginx-container"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-service"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
